55d2a21fca8be65b2f18df80f01876ee95dba82b
alvean <alvean.ekman@gmail.com>
2015-10-13 18:39:35 +0200

    automaiskt ifyllda fält på register
    går på mail

--- register.js
+++ register.js
@@ -11,6 +11,19 @@ Schema = {
         email: {
             type: String,
             regEx: SimpleSchema.RegEx.Email,
+            custom: function () {
+                if (Meteor.isClient && this.isSet) {
+                    var student = Students.findOne({email: this.value});
+                    if (student) {
+                        var form = $("form#registrationForm");
+
+                        form.find("input[name=name]").val(student.name);
+                        form.find("input[name=phone]").val(student.phone);
+                        form.find("input[name=prog]").val(student.prog);
+                        form.find("input[name=focus]").val(student.focus);
+                    }
+                }
+            },
             label: "Email"
         },
         prog: {
@@ -19,7 +32,7 @@ Schema = {
         },
         focus: {
             type: String,
-            label: "Inriktning, examensår"
+            label: "Inriktning, examensÃ¥r"
         },
     })
 };
@@ -32,25 +45,12 @@ if (Meteor.isClient) {
     AutoForm.hooks({
         registrationForm: {
             onSuccess: function(formType, result) {
+                Meteor.call('insertStudentPlayer', result.data);
+
                 // Redirect user to game
                 window.location = "/commitscore/" + result.token;
-                Students.insert({
-                   name: result.name,
-                   phone: result.phone,
-                   email: result.email,
-                   prog: result.prog,
-                   focus: result.focus
-
-                });
-
-                Players.insert({
-                    email: result.email,
-                    token: result.token,
-                    round: roundCounter,
-                    score: "0"
-                });
             }
-        },
+        }
     });
 
     Template.register.helpers({
@@ -66,7 +66,28 @@ if (Meteor.isServer) {
             check(doc, Schema.registrationSchema);
 
             var playerToken = Meteor.uuid();
-            return {token: playerToken};
+            return {token: playerToken, data: doc};
+        }
+    });
+
+    Meteor.methods({
+        insertStudentPlayer: function(result) {
+            console.log(result);
+
+            Students.insert({
+                name: result.name,
+                phone: result.phone,
+                email: result.email,
+                prog: result.prog,
+                focus: result.focus
+            });
+
+            Players.insert({
+                email: result.email,
+                token: result.token,
+                round: 0,
+                score: "0"
+            });
         }
     });
 }
